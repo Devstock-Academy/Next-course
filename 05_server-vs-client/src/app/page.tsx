66a7a66864376581f9a69caafe3c8df9e3baeeb8
RafalKielbasa <kielbasarafal92@gmail.com>
2025-03-12 17:49:14 +0100

    06_start-code

--- 05_server-vs-client/src/app/page.tsx
+++ 05_server-vs-client/src/app/page.tsx
@@ -1,3 +1,9 @@
+import ClientComponent from './ClientComponent'
+
 export default function Home() {
-  return <main className='mt-5 max-w-xl mx-auto'></main>
+  return (
+    <main className='mt-5 max-w-xl mx-auto'>
+      <ClientComponent />
+    </main>
+  )
 }
