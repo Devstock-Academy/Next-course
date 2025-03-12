d7a9b110a8378414e8496f1fb4b4771d19df6456
RafalKielbasa <kielbasarafal92@gmail.com>
2025-03-12 11:56:50 +0100

    03_sample-code

--- 03_SSR-vs-SPA/next-app/src/app/page.tsx
+++ 03_SSR-vs-SPA/next-app/src/app/page.tsx
@@ -1,7 +1,10 @@
+import ClientComponent from '@/components/ClientComponent'
+
 export default function Home() {
   return (
     <main className='p-5'>
       <h1 className='text-4xl font-bold'>Welcome To Task List</h1>
+      <ClientComponent />
     </main>
   )
 }
