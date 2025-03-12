d2fadaaf31644f4661571b8e9d51853acc24effd
RafalKielbasa <kielbasarafal92@gmail.com>
2025-03-12 15:07:46 +0100

    05-server-vs-client

--- 05_server-vs-client/src/app/ClientComponent.tsx
+++ 05_server-vs-client/src/app/ClientComponent.tsx
@@ -1,3 +1,10 @@
-export default function ClientComponent() {
-  return <div></div>
+'use client'
+
+export default function ClientComponent({ name }: { name: string }) {
+  return (
+    <div className='my-5'>
+      <h1 className='font-bold text-2xl'>Client Component</h1>
+      <div>{name}</div>
+    </div>
+  )
 }
--- 05_server-vs-client/src/app/page.tsx
+++ 05_server-vs-client/src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <main className='mt-5 max-w-xl mx-auto'></main>
+import ServerComponent from './ServerComponent'
+import ClientComponent from './ClientComponent'
+
+export default async function Home() {
+  const res = await fetch('https://reqres.in/api/users/2')
+  const { data } = await res.json()
+
+  return (
+    <main className='mt-5 max-w-xl mx-auto'>
+      <ServerComponent />
+      <ClientComponent name={`${data.first_name} ${data.last_name}`} />
+    </main>
+  )
 }
