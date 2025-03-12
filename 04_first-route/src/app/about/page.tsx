8d810a6fe2ead30f196cc3c5b79293f2d21da52b
RafalKielbasa <kielbasarafal92@gmail.com>
2025-03-12 12:59:51 +0100

    04-sample-code

--- 04_first-route/src/app/about/page.tsx
+++ 04_first-route/src/app/about/page.tsx
@@ -1,3 +1,8 @@
 export default function AboutRoute() {
-  return <main></main>
+  return (
+    <main>
+      <h1 className='text-4xl font-bold'>About Us</h1>
+      <div className='mt-5'>About Us and Our Awesome Application</div>
+    </main>
+  )
 }
--- 04_first-route/src/app/layout.tsx
+++ 04_first-route/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -24,10 +25,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className='px-2 md:px-5'>
+        <header className='text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex'>
+          <div className='flex flex-grow'>
+            <Link href='/'>Notes</Link>
+            <Link href='/about' className='ml-5 font-light'>
+              About
+            </Link>
+          </div>
+          <div></div>
+        </header>
+        <div className='flex flex-col md:flex-row'>
+          <div className='flex-grow'>{children}</div>
+        </div>
       </body>
     </html>
   )
