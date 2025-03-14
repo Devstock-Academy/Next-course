43eaad3f0de495d693c570165a1184c5afc314e0
RafalKielbasa <kielbasarafal92@gmail.com>
2025-03-14 22:52:18 +0100

    start-code

--- 10_store-data-id-database/src/app/page.tsx
+++ 10_store-data-id-database/src/app/page.tsx
@@ -1,12 +1,3 @@
-import TodoList from './TodoList'
-import { getTodos, createTodo, updateTodo } from './todo'
-
 export default async function Home() {
-  const todos = await getTodos()
-
-  return (
-    <div className='p-10'>
-      <TodoList createTodo={createTodo} todos={todos} updateTodo={updateTodo} />
-    </div>
-  )
+  return <div className='p-10'></div>
 }
