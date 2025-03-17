7fc37075b4e384a02b1dfb42970e4b2c293761b1
RafalKielbasa <kielbasarafal92@gmail.com>
2025-03-17 18:54:55 +0100

    13_start-code

--- 13_shadcn/src/app/page.tsx
+++ 13_shadcn/src/app/page.tsx
@@ -1,18 +1,10 @@
-import Blog from './Blog'
-import { getEntries } from './blog-api'
-import BlogForm from './BlogForm'
+
 
 export default async function Home() {
-  const entries = await getEntries()
 
   return (
     <div className='flex'>
-      <div className='w-1/2'>
-        <Blog entries={entries} />
-      </div>
-      <div className='w-1/2'>
-        <BlogForm />
-      </div>
+  
     </div>
   )
 }
